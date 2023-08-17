260e3c0012f10dcc307495b7ea41afa51247143d
reverb6821 <giuseppegigliottidev@gmail.com>
2023-08-17 17:26:55 +0200

    setted project folder

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,18 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
+  root: true,
+  env: { browser: true, es2020: true },
   extends: [
-    'plugin:react/recommended',
-    'standard'
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-  }
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
 }

306a0c9a91f415cdb40e9f06b97f1d6471befd8f
reverb6821 <giuseppegigliottidev@gmail.com>
2023-12-10 17:39:08 +0100

    fixed dependency

--- src/components/table/shared/ExportButton.tsx
+++ src/components/table/shared/ExportButton.tsx
@@ -1,12 +1,13 @@
-import * as FileSaver from "file-saver";
+import { DateTime } from 'luxon';
 import * as React from 'react';
+import { downloadExcel } from "react-export-table-to-excel";
 import { useTranslation } from 'react-i18next';
-import * as XLSX from "xlsx";
+
 
 import Button from '@/components/button/Button';
 
 interface ExportButtonProps {
-    data: string[] | undefined | object[],
+    data: { [key: string]: string | number | boolean; }[],
     headers: string[],
     model: string
 }
@@ -14,23 +15,23 @@ interface ExportButtonProps {
 const ExportButton: React.FC<ExportButtonProps> = ({ data, headers, model }) => {
     const { t } = useTranslation();
 
-    const fileType =
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
-    const fileExtension = ".xlsx";
-
-    const exportToCSV = (data, fileName: string) => {
-        const dataWithHeaders = [headers, ...data];
-        const ws = XLSX.utils.json_to_sheet(dataWithHeaders);
-        const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
-        const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
-        const newData = new Blob([excelBuffer], { type: fileType });
-        FileSaver.saveAs(newData, fileName + fileExtension);
-    };
-
+    function handleDownloadExcel() {
+        if (!data || !headers) {
+            return;
+        }
+        downloadExcel({
+          fileName: `${model}_${DateTime.now().toString()}`,
+          sheet: "react-export-table-to-excel",
+          tablePayload: {
+            header: headers,
+            body: data,
+          },
+        });
+      }
     return (
         <React.Fragment>
             <Button
-                onBtnClick={() => exportToCSV(data, `${model}_`)}
+                onBtnClick={() => handleDownloadExcel()}
                 btnStyle='primary--success'
                 type="button"
                 label={t('t.app.table.button.export')}

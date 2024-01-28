93189bc90a8d7e190d0638f3ccb4ce77eea7e3a8
reverb6821 <giuseppegigliottidev@gmail.com>
2024-01-28 12:24:40 +0100

    configured automatic test

---
+++ src/components/Tooltip/test/Tooltip.spec.tsx
@@ -0,0 +1,29 @@
+import { render, fireEvent } from "@testing-library/react";
+
+import Tooltip from "../Tooltip";
+
+describe("Tooltip", () => {
+    test('Tooltip shows the correct text when the mouse enters', async () => {
+        const tooltipText = 'Lorem Ipsum dolor si amet';
+        const { container } = render(<Tooltip tooltipText={tooltipText} />);
+        expect(container.querySelector('i')).toBeInTheDocument();
+      });
+
+    test('Tooltip shows the correct text when the mouse enters', async () => {
+        const tooltipText = 'Lorem Ipsum dolor si amet';
+        const { container, queryByText } = render(<Tooltip tooltipText={tooltipText} />);
+        const tooltipSelector = container.querySelector('i')
+        expect(queryByText(tooltipText)).toBeNull();
+        fireEvent.mouseEnter(tooltipSelector!);
+        expect(queryByText(tooltipText)).toBeInTheDocument();
+      });
+      
+      test('Tooltip disappears when the mouse exits', async () => {
+        const tooltipText = 'Lorem Ipsum dolor si amet';
+        const { container, queryByText } = render(<Tooltip tooltipText={tooltipText} />);
+        const tooltipSelector = container.querySelector('i')
+        fireEvent.mouseEnter(tooltipSelector!);
+        fireEvent.mouseLeave(tooltipSelector!);
+        expect(queryByText(tooltipText)).toBeNull();
+      });
+});
